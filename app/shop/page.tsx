c57af274a82eb5bd6d69915f8b71c555cadf2969
Abdul-Azeem Traoure Mohammed <mohammedtraoure85@gmail.com>
2026-03-03 19:04:51 +0000

    type safety

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -2,13 +2,13 @@ import { ProductGrid } from '../../components/ProductGrid'
 import { getSnacks } from '../actions/snacks'
 
 export default async function ShopPage() {
-  const snacks = await getSnacks()
+  const snacks = (await getSnacks()) as any[]
   return (
     <div className="flex min-h-screen flex-col bg-zinc-50 dark:bg-black p-4 md:p-8">
       <h1 className="text-3xl font-black uppercase tracking-tighter mb-8">
         All Snacks
       </h1>
-      <ProductGrid snacks={snacks}/>
+      <ProductGrid snacks={snacks} />
     </div>
   )
 }
